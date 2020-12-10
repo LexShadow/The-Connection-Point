4df3f2f1cfceee5b374f9d320a8318588461909d
LexShadow <fox.moppetmites@gmail.com>
2020-12-10 10:40:16 +0000

    Better configration support 0.0.0.3
    Added support for better configration in the index page.

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
-include('./config/Parsedown.php');
-include('./config/ParsedownExtra.php');
-include('./config/ParsedownExtraFix.php');
+#Basic Setup
+define('APP_ID', 'The Connection Point');
+define('APP_VERSION', '0.0.0.3');
+define('BASE_URL', './');
+define('SITE_TITLE', APP_ID);
+define('SITE_SUB_TITLE', 'Random blog about random things');
+define('SITE_WEBMASTER_NAME', 'Admin');
+define('SITE_WEBMASTER_EMAIL', 'noemail@nodomain.ltd');
 
-$base_url = './'; // URL of the homepage. Can be absolute or relative.
-$blog_title = 'The Connection Point';
+#include files
+
+if(file_exists('./config/Parsedown.php')){
+	$stop = false;
+	include('./config/Parsedown.php');
+}else{
+	$stop = true;
+}
+if(file_exists('./config/ParsedownExtra.php')){
+	$stop = false;
+	include('./config/ParsedownExtra.php');
+}else{
+	$stop = true;
+}
+if(file_exists('./config/ParsedownExtraFix.php')){
+	$stop = false;
+	include('./config/ParsedownExtraFix.php');
+}else{
+	$stop = true;
+}
 $blog_sub_title = 'Random blog about random things';
 $contact_email = 'noemail@nodomain.ltd';
 $headder = '
@@ -34,7 +57,7 @@ if(!empty($_GET['post'])){
 		$content = '
 			<h2>Not Found</h2>
 			<p>Sorry, couldn\'t find a post with that name. Please try again, or go to the 
-			<a href="' . $base_url . '">home page</a> to select a different post.</p>';
+			<a href="' . BASE_URL . '">home page</a> to select a different post.</p>';
 	}
 	$contentfooter = '	<footer>
 		This blog does not offer comment functionality. we are working on this.
@@ -43,47 +66,51 @@ if(!empty($_GET['post'])){
 		This blog does not offer comment functionality. If you\'d like to discuss any of the topics 
 		written about here, you can <a href="mailto:' . $contact_email . '">send an email</a>.
 	</footer>';	
-} else {
-	$content = "";
-	$GrabThis = array_slice(scandir(__DIR__.'/content/', 1), 0, 6);
-	foreach($GrabThis as $file) {
-		if ($file[0] != '.') {
-			if(!is_dir(__DIR__.'/content/' . $file)){
-				
-				$filename_no_ext = $file;
-				$filename_no_extShow = str_replace(".txt", "", $file);
-				if(strlen($filename_no_extShow) > 4){
-					$file_path = __DIR__.'/content/' . $file;
-					$files = fopen($file_path, 'r');
-					//$post_title = trim(fgets($files),'#');
-					$line = 0;
-					while(($buffer = fgets($files)) !== FALSE){
-						if($line == 0){
-							$post_title = trim($buffer,'#');
+}else{
+	if(!$stop){
+		$content = "";
+		$GrabThis = array_slice(scandir(__DIR__.'/content/', 1), 0, 6);
+		foreach($GrabThis as $file){
+			if($file[0] != '.'){
+				if(!is_dir(__DIR__.'/content/' . $file)){
+					$filename_no_ext = $file;
+					$filename_no_extShow = str_replace(".txt", "", $file);
+					if(strlen($filename_no_extShow) > 4){
+						$file_path = __DIR__.'/content/' . $file;
+						$files = fopen($file_path, 'r');
+						$line = 0;
+						while(($buffer = fgets($files)) !== FALSE){
+							if($line == 0){
+								$post_title = trim($buffer,'#');
+							}
+							if($line == 1){
+								$post_auther = $buffer;
+								break;
+							}
+							$line++;
 						}
-						if($line == 1){
-							$post_auther = $buffer;
-							break;
-						}
-						$line++;
-					}
-					fclose($files);
-					$content .= '<!-- #Blog post -->
-					<div class="blog-post">
-						<h2 class="blog-post-title"><a href="'.$base_url.'?post='.$filename_no_ext.'">'.$post_title.'</a></h2>
-						<p class="blog-post-meta">' . $post_auther . '</p>
-					</div>';
-					$fav = substr($content, strpos($content, '{fas}')+5);
-					$fav = substr($fav, 0, strpos($fav, '{/fas}'));
-					while($fav != ""){
-						$favplace = ' <i class="fas ' . $fav . '"></i> ';
-						$content = str_replace("{fas}" . $fav . "{/fas}", $favplace, $content);
+						fclose($files);
+						$content .= '<!-- #Blog post -->
+						<div class="blog-post">
+							<h2 class="blog-post-title"><a href="' . BASE_URL . '?post='.$filename_no_ext.'">'.$post_title.'</a></h2>
+							<p class="blog-post-meta">' . $post_auther . '</p>
+						</div>';
 						$fav = substr($content, strpos($content, '{fas}')+5);
 						$fav = substr($fav, 0, strpos($fav, '{/fas}'));
+						while($fav != ""){
+							$favplace = ' <i class="fas ' . $fav . '"></i> ';
+							$content = str_replace("{fas}" . $fav . "{/fas}", $favplace, $content);
+							$fav = substr($content, strpos($content, '{fas}')+5);
+							$fav = substr($fav, 0, strpos($fav, '{/fas}'));
+						}
 					}
 				}
 			}
 		}
+	}else{
+			$content = '
+			<h2>Not Found</h2>
+			<p>Missing files needed, please check the config files for Parsedown, ParsedownExtra & ParsedownExtraFix.</p>';	
 	}
 	$contentfooter = '';
 }
@@ -91,7 +118,7 @@ if(!empty($_GET['post'])){
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php if ( !empty($_GET['post']) ) { echo $post_title.' - '; } ?><?php echo $blog_title; ?></title>
+		<title><?php if ( !empty($_GET['post']) ) { echo $post_title.' - '; } ?><?php echo SITE_TITLE; ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="./css/style.css?<?php echo rand(1000, 999999999); ?>" />
 		<link rel="stylesheet" href="./css/bootstrap.css" />
@@ -125,8 +152,8 @@ if(!empty($_GET['post'])){
 		<hr />
 		<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
 			<div class="col-md-6 px-0">
-				<h1 class="display-4 font-italic"><a href="<?php echo $base_url; ?>"><?php echo $blog_title; ?></a></h1>
-				<p class="lead my-3"><?php echo $blog_title; ?></p>
+				<h1 class="display-4 font-italic"><a href="<?php echo $base_url; ?>"><?php echo SITE_TITLE; ?></a></h1>
+				<p class="lead my-3"><?php echo SITE_SUB_TITLE; ?></p>
 			</div>
 		</div>	  
 	</div>
@@ -145,7 +172,7 @@ if(!empty($_GET['post'])){
         <aside class="col-md-4 blog-sidebar">
           <div class="p-3 mb-3 bg-light rounded">
             <h4 class="font-italic">News</h4>
-            <p class="mb-0">The Connection Point<br/> Version 0.0.0.2</p>
+            <p class="mb-0">The Connection Point<br/> <?php echo APP_VERSION;?></p>
           </div>
           <div class="p-3">
             <h4 class="font-italic">Pages</h4>
@@ -172,7 +199,7 @@ if(!empty($_GET['post'])){
       </div>
     </main>
     <footer class="blog-footer">
-      <p><i class="fas fa-copyright"></i> <?php echo $blog_title;?> 2020</p>
+      <p><i class="fas fa-copyright"></i> <?php echo SITE_TITLE;?> 2020</p>
       <p>
         <a href="#">Back to top</a>
       </p>
